71a7912c4e492b78651690472340ba03e30fd906
JanzenDC <janzendelacruz28@gmail.com>
2025-03-07 16:30:41 +0800

    Add current date and time display to MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,6 +5,10 @@
         <q-toolbar-title>
           CC
         </q-toolbar-title>
+        <q-space />
+        <div class="q-mr-md text-caption">
+          {{ currentDateTime }}
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -15,5 +19,26 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const currentDateTime = ref('');
+
+const updateDateTime = () => {
+  const now = new Date();
+  currentDateTime.value = now.toLocaleString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
+};
+
+onMounted(() => {
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+});
 </script>
